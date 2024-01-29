1f82bc6ad3fc883dc6a6cbf6a31defdcfcacf06b
Aman-Raut <amanraut13@gmail.com>
2024-01-30 02:22:18 +0530

    modified:   src/App.css 	modified:   src/component/Home.js 	modified:   src/component/NavBar.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,7 +5,7 @@ function Home(){
         
         <h1>Home Page</h1>
         <p>This application is so awesome. that i can't stop myself
-            from not learning more. so easy to use and very my fast.
+            from not learning more. so easy to use and quite fast.
             everything works fine till now. gonna jump on backend i.e on 
             node.js sooner and then on mongo DB. hope for the best. 
             </p>
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,14 +1,13 @@
-import {BrowserRouter,Route,Routes,Link} from 'react-router-dom';
+import {BrowserRouter,Route,Routes,Link, NavLink} from 'react-router-dom';
 
 function NavBar(){
     return<>
-    <Link to='/about' >About</Link>
-     &nbsp;&nbsp;&nbsp;
-    <Link to='/home' >Home</Link>
-    &nbsp;&nbsp;&nbsp;
-    <Link to='/user/aman' >Aman</Link>
-    &nbsp; 
-    <Link to='/user/manu' >Manu</Link>
+    <ul className='navbar'>
+        <li><NavLink className='nav-bar-link' style={{color:'red'}} to='/about' >About</NavLink></li>
+        <li><NavLink className='nav-bar-link' to='/home' >Home</NavLink></li>
+        <li> <NavLink className='nav-bar-link' to='/user/aman' >Aman</NavLink></li>
+        <li><NavLink className='nav-bar-link' to='/user/manu' >Manu</NavLink></li>
+    </ul>
     </>
 }
 export default NavBar;
